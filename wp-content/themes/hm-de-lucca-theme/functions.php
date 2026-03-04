9c2cb4b0df5791e703dfff5c1c49dfbe7a72f673
Matheus Malaquias <contato.matheusmalaquias@gmail.com>
2026-03-04 12:24:09 -0300

    create custom theme

---
+++ wp-content/themes/hm-de-lucca-theme/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function hm_theme_scripts() {
+    wp_enqueue_style('main-style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'hm_theme_scripts');
---
+++ wp-content/themes/hm-de-lucca-theme/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>Site HM De Lucca</h1>
+
+<?php get_footer(); ?>
